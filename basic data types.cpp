6bc041d37850275c8cf8b43f52cd3ce81e604066
Ayush Singh <singhayush.mth@gmail.com>
2022-01-24 23:12:21 +0530

    Create basic data types.cpp

---
+++ basic data types.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long l;
+    char c;
+    float a;
+    double b;
+    cin>>i>>l>>c>>a>>b;
+    cout<<i<<endl<<l<<endl<<c<<endl;
+    cout<<fixed<<setprecision(3)<<a<<"\n";
+    cout<<fixed<<setprecision(9)<<b<<"\n";
+    return 0;
+}
